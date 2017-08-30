9afbe0e5f9781fa1068aa5a1167eb6e91b5efad9
Upul Bandara <upulbandara@gmail.com>
2017-08-30 09:30:51 -0500

    Added: model save and restore

--- fcn.py
+++ fcn.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import tensorflow as tf
 import time
+import numpy as np
 
 import helper
 from loss import logistic_loss
@@ -15,19 +16,19 @@ from model_utils import preprocess
 
 
 class FCN:
-    def __init__(self, input_shape, batch_size=8, num_classes=2):
+    def __init__(self, input_shape, num_train_examples, viz_dir, batch_size=8, num_classes=2):
+        self.images_batch, self.labels_batch, self.images_viz, self.dropout, self.global_step = self._build_placeholders(
+            input_shape)
         self.input_shape = input_shape
-        X, Y, dropout = self._build_placeholders(input_shape)
-        self.img_placeholder = X
-        self.lbl_placeholder = Y
-        self.dropout = dropout
+        self.viz_dir = viz_dir
+        self.num_train_examples = num_train_examples
         self.batch_size = batch_size
         self.n_classes = num_classes
         self.sess = tf.Session()
         self.logits = self.build()
 
     def build(self):
-        images = preprocess(self.img_placeholder)
+        images = preprocess(self.images_batch)
 
         conv1_1 = conv_layer(images, 'conv1_1_W', 'conv1_1_b', name='conv1_1')
         conv1_2 = conv_layer(conv1_1, 'conv1_2_W', 'conv1_2_b', name='conv1_2')
@@ -57,7 +58,7 @@ class FCN:
         fc_3 = fully_collected_layer(fc_2, 'fc_3', self.dropout)
 
         # New we start upsampling and skip layer connections.
-        img_shape = tf.shape(self.img_placeholder)
+        img_shape = tf.shape(self.images_batch)
         dconv3_shape = tf.stack([img_shape[0], img_shape[1], img_shape[2], self.n_classes])
         upsample_1 = upsample_layer(fc_3, dconv3_shape, self.n_classes, 'upsample_1', 32)
 
@@ -71,38 +72,80 @@ class FCN:
         return logit
 
     def optimize(self, batch_generator, learning_rate=1e-5, keep_prob=0.75, num_epochs=1):
-        loss = logistic_loss(logits=self.logits, labels=self.lbl_placeholder, n_classes=self.n_classes)
-        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        loss = logistic_loss(logits=self.logits, labels=self.labels_batch, n_classes=self.n_classes)
+        summary_op = self._build_summary(loss=loss)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=self.global_step)
+
+        validation_img_summary_op = tf.summary.image('validation_img', self.images_viz)
 
         self.sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        # to visualize using TensorBoard
+        writer = tf.summary.FileWriter('./graphs/kitti/', self.sess.graph)
+        ckpt = tf.train.get_checkpoint_state(os.path.dirname('./checkpoints/kitti/'))
+        if ckpt and ckpt.model_checkpoint_path:
+            print('Graph is available in hard disk. Hence, loading it.')
+            saver.restore(self.sess, ckpt.model_checkpoint_path)
 
-        for epoch in range(num_epochs):
-            itr = 0
-            for image, gt_image in batch_generator(self.batch_size):
-                _, loss_val = self.sess.run([optimizer, loss],
-                                            feed_dict={self.img_placeholder: image,
-                                                       self.lbl_placeholder: gt_image,
-                                                       self.dropout: keep_prob})
-                if (itr < 10) or (itr < 100 and itr % 10 == 0) or \
-                        (itr < 1000 and itr % 100 == 0) or (itr >= 1000 and itr % 200 == 0):
-                    print('epoch: {0:>3d}  iter: {1:>4d}  loss: {2:>8.4e}'.format(epoch, itr, loss_val))
-                itr += 1
+        initial_step = self.global_step.eval(session=self.sess)
+        num_batches = int(self.num_train_examples / self.batch_size)
+        for itr in range(initial_step, num_batches * num_epochs):
+            image, gt_image = next(batch_generator(self.batch_size))
+            _, loss_val, summary = self.sess.run([optimizer, loss, summary_op],
+                                                 feed_dict={self.images_batch: image,
+                                                            self.labels_batch: gt_image,
+                                                            self.dropout: keep_prob})
+            writer.add_summary(summary, global_step=itr)
+
+            if (itr < 10) or (itr < 100 and itr % 10 == 0) or \
+                    (itr < 1000 and itr % 100 == 0) or (itr >= 1000 and itr % 200 == 0):
+                print('epoch: {0:>3d}  iter: {1:>4d}  loss: {2:>8.4e}'.format(itr, itr, loss_val))
+
+            if itr % 10 == 0:
+                viz_images = self.training_visulize()
+                tt = self.sess.run([validation_img_summary_op], feed_dict={self.images_viz: viz_images})
+                writer.add_summary(tt[0], itr)
+
+            if itr % self.num_train_examples == 0:
+                print('At iteration: {} save a checkpoint'.format(itr))
+                saver.save(self.sess, './checkpoints/kitti', itr)
 
     def inference(self, runs_dirs, data_dirs):
         reshape_logits = tf.reshape(self.logits, (-1, self.n_classes))
         helper.save_inference_samples(runs_dirs, data_dirs, self.sess, self.input_shape, reshape_logits, self.dropout,
-                                      self.img_placeholder)
+                                      self.images_batch)
 
     def close_session(self):
         self.sess.close()
+
+    def training_visulize(self):
+        reshape_logits = tf.reshape(self.logits, (-1, self.n_classes))
+        viz_images = []
+        img_output = helper.gen_test_output(self.sess, reshape_logits, self.dropout, self.images_batch, self.viz_dir,
+                                            self.input_shape)
+        for _, ouput in img_output:
+            viz_images.append(ouput)
+        return np.array(viz_images)
 
     @staticmethod
     def _build_placeholders(shape):
         with tf.name_scope('data'):
             X = tf.placeholder(tf.float32, [None, shape[0], shape[1], 3], name='X_placeholder')
             Y = tf.placeholder(tf.float32, [None, shape[0], shape[1], 2], name='Y_placeholder')
+            X_viz = tf.placeholder(tf.float32, [None, shape[0], shape[1], 3], name='X_valid_placeholder')
+
         dropout = tf.placeholder(tf.float32, name='dropout')
-        return X, Y, dropout
+        global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
+
+        return X, Y, X_viz, dropout, global_step
+
+    @staticmethod
+    def _build_summary(loss):
+        with tf.name_scope('summaries'):
+            tf.summary.scalar('loss', loss)
+            tf.summary.histogram('histogram loss', loss)
+            return tf.summary.merge_all()
 
 
 if __name__ == '__main__':
@@ -110,9 +153,11 @@ if __name__ == '__main__':
     images_per_batch = 8
     data_dir = './data'
     runs_dir = './runs'
+    viz_dir = './data/data_road/validating'
     input_size = (160, 576)
+    num_train_examples = 289
 
     get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), input_size)
-    fc_network = FCN(input_size, images_per_batch, num_classes)
-    fc_network.optimize(get_batches_fn, num_epochs=5)
+    fc_network = FCN(input_size, num_train_examples, viz_dir, images_per_batch, num_classes)
+    fc_network.optimize(get_batches_fn, num_epochs=1)
     fc_network.inference(runs_dir, data_dir)
--- helper.py
+++ helper.py
@@ -105,7 +105,7 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
     Generate test output using the test images
     :param sess: TF session
     :param logits: TF Tensor for the logits
-    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param keep_prob: TF Placeholder for the dropout keep probability
     :param image_pl: TF Placeholder for the image placeholder
     :param data_folder: Path to the folder that contains the datasets
     :param image_shape: Tuple - Shape of image
@@ -119,7 +119,7 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
             {keep_prob: 1.0, image_pl: [image]})
         im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
         segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
-        mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+        mask = np.dot(segmentation, np.array([[221, 28, 199, 127]]))
         mask = scipy.misc.toimage(mask, mode="RGBA")
         street_im = scipy.misc.toimage(image)
         street_im.paste(mask, box=None, mask=mask)
